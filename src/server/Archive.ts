d290054e5cf74269be496a2581498c2dca7014a6
evanpelle <evanpelle@gmail.com>
2024-12-09 13:14:59 -0800

    catch and log archive failure to prevent server crash

--- src/server/Archive.ts
+++ src/server/Archive.ts
@@ -4,10 +4,14 @@ import { Storage } from '@google-cloud/storage';
 const storage = new Storage();
 
 export async function archive(gameRecord: GameRecord) {
-    console.log(`writing game ${gameRecord.id} to gcs`)
-    const bucket = storage.bucket("openfront-games");
-    const file = bucket.file(gameRecord.id);
-    await file.save(JSON.stringify(GameRecordSchema.parse(gameRecord)), {
-        contentType: 'application/json'
-    });
+    try {
+        console.log(`writing game ${gameRecord.id} to gcs`)
+        const bucket = storage.bucket("openfront-games");
+        const file = bucket.file(gameRecord.id);
+        await file.save(JSON.stringify(GameRecordSchema.parse(gameRecord)), {
+            contentType: 'application/json'
+        });
+    } catch (error) {
+        console.log(`error writing to gcs: ${error}`)
+    }
 }
