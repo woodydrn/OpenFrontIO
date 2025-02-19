e67cffa3145ec43b5f29fc2a2d8b9cc944127394
SunnyBoyWTF <Tylercoolguy1234@gmail.com>
2025-02-19 10:31:41 -0500

    Update Maps.ts to fix bugs

--- src/client/utilities/Maps.ts
+++ src/client/utilities/Maps.ts
@@ -5,7 +5,7 @@ import mena from "../../../resources/maps/Mena.png";
 import northAmerica from "../../../resources/maps/NorthAmerica.png";
 import blackSea from "../../../resources/maps/BlackSea.png";
 import africa from "../../../resources/maps/Africa.png";
- main
+
 import { GameMapType } from "../../core/game/Game";
 
 export function getMapsImage(map: GameMapType): string {
@@ -22,10 +22,8 @@ export function getMapsImage(map: GameMapType): string {
       return northAmerica;
     case GameMapType.BlackSea:
       return blackSea;
-    case GameMapType.Africa
-      return Africa;
-
-      main
+    case GameMapType.Africa:
+      return africa;  // Corrected the case for Africa
     default:
       return "";
   }
