33c634f928da081807881cf715a946daf886ba86
Evan <evanpelle@gmail.com>
2025-02-20 19:16:31 -0800

    allow ü in schema

--- src/core/Schemas.ts
+++ src/core/Schemas.ts
@@ -102,7 +102,7 @@ const SafeString = z
   // Remove common dangerous characters and patterns
   // The weird \u stuff is to allow emojis
   .regex(
-    /^[a-zA-Z0-9\s.,!?@#$%&*()-_+=\[\]{}|;:"'\/\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]]+$/,
+    /^[a-zA-Z0-9\s.,!?@#$%&*()-_+=\[\]{}|;:"'\/\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]|üÜ]+$/,
   )
   // Reasonable max length to prevent DOS
   .max(1000);
