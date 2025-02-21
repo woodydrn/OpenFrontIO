74f56d8faca980a1bfa5b2f68729e6f8ae84f1f0
Evan <evanpelle@gmail.com>
2025-02-20 19:10:40 -0800

    allow ü

--- src/core/validations/username.ts
+++ src/core/validations/username.ts
@@ -13,7 +13,7 @@ const matcher = new RegExpMatcher({
 export const MIN_USERNAME_LENGTH = 3;
 export const MAX_USERNAME_LENGTH = 27;
 
-const validPattern = /^[a-zA-Z0-9_\[\] 🐈🍀]+$/u;
+const validPattern = /^[a-zA-Z0-9_\[\] 🐈🍀üÜ]+$/u;
 
 const shadowNames = [
   "NicePeopleOnly",
