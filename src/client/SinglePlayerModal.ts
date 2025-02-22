240882f14b2153051f133cc2e50e3ff206f0296a
SunnyBoyWTF <Tylercoolguy1234@gmail.com>
2025-02-22 10:36:56 -0500

    Update SinglePlayerModal.ts

--- src/client/SinglePlayerModal.ts
+++ src/client/SinglePlayerModal.ts
@@ -297,7 +297,7 @@ export class SinglePlayerModal extends LitElement {
                     @change=${this.handleDisableBotsChange}
                     .checked=${this.disableBots}
                   />
-                  <div class="option-card-title">Disable Nations</div>
+                  <div class="option-card-title">Disable Bots</div>
                 </label>
 
                 <label
@@ -311,7 +311,7 @@ export class SinglePlayerModal extends LitElement {
                     @change=${this.handleDisableNPCsChange}
                     .checked=${this.disableNPCs}
                   />
-                  <div class="option-card-title">Disable NPCs</div>
+                  <div class="option-card-title">Disable Nations</div>
                 </label>
 
                 <label
