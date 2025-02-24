4ccd6071c6843f333bc8783f3a1815db1ebdd43a
Evan <evanpelle@gmail.com>
2025-02-23 20:37:39 -0800

    connect ocean on asia map to allow boats

--- src/scripts/TerrainMapGenerator.ts
+++ src/scripts/TerrainMapGenerator.ts
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const mapName = "Europe";
+const mapName = "Asia";
 
 interface Coord {
   x: number;
