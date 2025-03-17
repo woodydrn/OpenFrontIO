f0b2d0d065f5a2a24efb8e9eba4d1af9bb268ba4
Evan <evanpelle@gmail.com>
2025-03-16 20:40:56 -0700

    remove bun build from deployment

--- deploy.sh
+++ deploy.sh
@@ -27,9 +27,6 @@ if [ $# -ne 1 ] || ([ "$1" != "staging" ] && [ "$1" != "prod" ] && [ "$1" != "al
     exit 1
 fi
 
-# TODO: fix this - need to build before creating the image
-bun run build-prod
-
 ENV=$1
 VERSION_TAG="latest"
 DOCKER_REPO=""
@@ -79,7 +76,6 @@ GIT_COMMIT=$(git rev-parse HEAD 2>/dev/null || echo "unknown")
 echo "Git commit: $GIT_COMMIT"
 
 docker buildx build \
-  --no-cache \
   --platform linux/amd64 \
   --build-arg GIT_COMMIT=$GIT_COMMIT \
   -t $DOCKER_USERNAME/$DOCKER_REPO:$VERSION_TAG \
