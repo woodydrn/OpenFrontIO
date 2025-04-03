2fd7bbaddf8dfd0af245722ce44c29ce37131dcd
Evan <evanpelle@gmail.com>
2025-04-03 16:16:23 -0700

    fix build, rename GameStartingModal.ts

--- src/client/gameStartingModal.ts
+++ src/client/gameStartingModal.ts
@@ -1,6 +1,6 @@
 import { LitElement, css, html } from "lit";
 import { customElement, state } from "lit/decorators.js";
-import { translateText } from "../client/Utils";
+import { translateText } from "./Utils";
 
 @customElement("game-starting-modal")
 export class GameStartingModal extends LitElement {
