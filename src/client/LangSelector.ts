c5c3dddb4edd52bfb7f1d911f774bf6bc3e374f3
Duwibi <86431918+Duwibi@users.noreply.github.com>
2025-04-16 04:25:29 +0300

    Update Translations (#498)
    ## Description:
    This PR updates the following languages to have the latest translations:
    Bulgarian, Dutch, German, French, Esperanto, Polish, Russian and
    Ukrainian. It adds the following new languages: Italian, Brazilian
    Portuguese, Serbo-Croatian, Turkish, Hindi and Bengali. It also adds a
    flag and integrates Esperanto.
    
    ## Please complete the following:
    
    - [x] I have added screenshots for all UI updates
    - [x] I confirm I have thoroughly tested these changes and take full
    responsibility for any bugs introduced
    - [x] I understand that submitting code with bugs that could have been
    caught through manual testing blocks releases and new features for all
    contributors
    
    Fixes #499 
    
    ## Please put your Discord username so you can be contacted if a bug or
    regression is found:
    
    Nikola123

--- src/client/LangSelector.ts
+++ src/client/LangSelector.ts
@@ -3,14 +3,21 @@ import { customElement, state } from "lit/decorators.js";
 import "./LanguageModal";
 
 import bg from "../../resources/lang/bg.json";
+import bn from "../../resources/lang/bn.json";
 import de from "../../resources/lang/de.json";
 import en from "../../resources/lang/en.json";
+import eo from "../../resources/lang/eo.json";
 import es from "../../resources/lang/es.json";
 import fr from "../../resources/lang/fr.json";
+import hi from "../../resources/lang/hi.json";
+import it from "../../resources/lang/it.json";
 import ja from "../../resources/lang/ja.json";
 import nl from "../../resources/lang/nl.json";
 import pl from "../../resources/lang/pl.json";
+import pt_br from "../../resources/lang/pt_br.json";
 import ru from "../../resources/lang/ru.json";
+import sh from "../../resources/lang/sh.json";
+import tr from "../../resources/lang/tr.json";
 import uk from "../../resources/lang/uk.json";
 
 @customElement("lang-selector")
@@ -26,14 +33,21 @@ export class LangSelector extends LitElement {
 
   private languageMap: Record<string, any> = {
     bg,
+    bn,
     de,
     en,
     es,
+    eo,
     fr,
+    it,
+    hi,
     ja,
     nl,
     pl,
+    pt_br,
     ru,
+    sh,
+    tr,
     uk,
   };
 
