b56764ce575c20a22509a5141007dfea97e7568d
Scott Anderson <scottanderson@users.noreply.github.com>
2025-05-01 15:22:20 -0400

    Update JWT schema (#632)
    ## Description:
    
    - Handle the new `sub` encoding
    - Decode the new `rol` claim
    - Related to https://github.com/openfrontio/infra/pull/25
    
    ## Please complete the following:
    
    - [x] I have added screenshots for all UI updates
    - [x] I confirm I have thoroughly tested these changes and take full
    responsibility for any bugs introduced
    - [x] I understand that submitting code with bugs that could have been
    caught through manual testing blocks releases and new features for all
    contributors
    
    Co-authored-by: Scott Anderson <662325+scottanderson@users.noreply.github.com>

--- src/client/ApiSchemas.ts
+++ src/client/ApiSchemas.ts
@@ -1,12 +1,32 @@
 import { z } from "zod";
+import { base64urlToUuid } from "./Base64";
 
 export const TokenPayloadSchema = z.object({
   jti: z.string(),
-  sub: z.string().uuid(),
+  sub: z
+    .string()
+    .refine(
+      (val) => {
+        const uuid = base64urlToUuid(val);
+        return uuid != null;
+      },
+      {
+        message: "Invalid base64-encoded UUID",
+      },
+    )
+    .transform((val) => {
+      const uuid = base64urlToUuid(val);
+      if (!uuid) throw new Error("Invalid base64 UUID");
+      return uuid;
+    }),
   iat: z.number(),
   iss: z.string(),
   aud: z.string(),
   exp: z.number(),
+  rol: z
+    .string()
+    .optional()
+    .transform((val) => val.split(",")),
 });
 export type TokenPayload = z.infer<typeof TokenPayloadSchema>;
 
