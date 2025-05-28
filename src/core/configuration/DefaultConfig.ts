5136f8edc273441b96bdf16bd1f9bd9a2b7e70c3
1brucben <1benjbruce@gmail.com>
2025-05-28 22:59:20 +0200

    Meta Adjustments from [UN] clan test (#932)
    ## Description:
    The changes seem necessary to reduce snowballing in v23 and strengthen
    ports.
    ## Please complete the following:
    
    - [x] I have added screenshots for all UI updates
    - [x] I confirm I have thoroughly tested these changes and take full
    responsibility for any bugs introduced
    - [x] I understand that submitting code with bugs that could have been
    caught through manual testing blocks releases and new features for all
    contributors
    
    ## Please put your Discord username so you can be contacted if a bug or
    regression is found:
    
    1brucben

--- src/core/configuration/DefaultConfig.ts
+++ src/core/configuration/DefaultConfig.ts
@@ -66,9 +66,9 @@ const numPlayersConfig = {
 } as const satisfies Record<GameMapType, [number, number, number]>;
 
 const TERRAIN_EFFECTS = {
-  [TerrainType.Plains]: { mag: 0.85, speed: 0.8 },
-  [TerrainType.Highland]: { mag: 1, speed: 1 },
-  [TerrainType.Mountain]: { mag: 1.2, speed: 1.3 },
+  [TerrainType.Plains]: { mag: 0.9, speed: 0.8 },
+  [TerrainType.Highland]: { mag: 1.1, speed: 1 },
+  [TerrainType.Mountain]: { mag: 1.3, speed: 1.25 },
 } as const;
 
 export abstract class DefaultServerConfig implements ServerConfig {
@@ -276,7 +276,7 @@ export class DefaultConfig implements Config {
     return 10000 + 150 * Math.pow(dist, 1.1);
   }
   tradeShipSpawnRate(numberOfPorts: number): number {
-    return Math.round(10 * Math.pow(numberOfPorts, 0.6));
+    return Math.round(10 * Math.pow(numberOfPorts, 0.5));
   }
 
   unitInfo(type: UnitType): UnitInfo {
@@ -647,7 +647,7 @@ export class DefaultConfig implements Config {
     // smaller countries recieve a boost to pop growth to speed up early game
     const baseAdditionRate = 10;
     const basePopGrowthRate = 1300 / max + 1 / 140;
-    const reproductionPop = 0.8 * player.troops() + 1.2 * player.workers();
+    const reproductionPop = 0.85 * player.troops() + 1.15 * player.workers();
     let toAdd = baseAdditionRate + basePopGrowthRate * reproductionPop;
     const totalPop = player.totalPopulation();
     const ratio = 1 - totalPop / max;
