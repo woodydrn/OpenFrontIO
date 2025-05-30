ca0a848f9fe2a8b55871b12753b40111b9805000
evanpelle <openfrontio@gmail.com>
2025-05-29 19:44:53 -0700

    show news button

--- src/client/Main.ts
+++ src/client/Main.ts
@@ -75,7 +75,7 @@ class Client {
     }
 
     // Comment out to show news button.
-    newsButton.hidden = true;
+    // newsButton.hidden = true;
 
     const langSelector = document.querySelector(
       "lang-selector",
