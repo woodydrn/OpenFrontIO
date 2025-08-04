3c63e3ffd856c1b621a02bfadb90c5b14cf44d11
evanpelle <evanpelle@gmail.com>
2025-08-04 16:32:03 -0700

    Use OR instead of nullish operator on TokenSchema (#1702)
    ## Description:
    
    PersistentIdSchema.safeParse(v).success returns a boolean not
    undefined/null.
    
    ## Please complete the following:
    
    - [x] I have added screenshots for all UI updates
    - [x] I process any text displayed to the user through translateText()
    and I've added it to the en.json file
    - [x] I have added relevant tests to the test directory
    - [x] I confirm I have thoroughly tested these changes and take full
    responsibility for any bugs introduced
    - [x] I have read and accepted the CLA agreement (only required once).
    
    ## Please put your Discord username so you can be contacted if a bug or
    regression is found:
    
    evan

--- src/core/Schemas.ts
+++ src/core/Schemas.ts
@@ -171,7 +171,7 @@ const TokenSchema = z
   .string()
   .refine(
     (v) =>
-      PersistentIdSchema.safeParse(v).success ??
+      PersistentIdSchema.safeParse(v).success ||
       JwtTokenSchema.safeParse(v).success,
     {
       message: "Token must be a valid UUID or JWT",
